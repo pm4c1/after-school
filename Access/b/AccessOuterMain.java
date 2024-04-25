d0e1051e4101b09a1cbd056fec0b0617adf482ca
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-04-25 17:58:11 +0900

    Create AccessOuterMain.java

---
+++ Access/b/AccessOuterMain.java
@@ -0,0 +1,23 @@
+package access.b;
+
+import access.a.AccessData;
+
+public class AccessOuterMain {
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+
+        // public
+        data.publicField = 1;
+        data.publicMethod();
+
+        // default
+        // data.defaultField = 2;
+        // data.defaultField();
+
+        // privata - 다른 클래스에서 호출 불가
+        // data.privataFiled = 3;
+        // data.privateMethod();
+
+        data.innerAccess();
+    }
+}
