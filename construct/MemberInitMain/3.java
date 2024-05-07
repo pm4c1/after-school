78c415be565b4721433a9a07bd37d6f1c02d7aea
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-05-07 16:29:48 +0900

    Create 3.java

---
+++ construct/MemberInitMain/3.java
@@ -0,0 +1,21 @@
+package construct;
+
+public class MemberInitMain3 {
+    public static void main(String[] args) {
+        MemberInit member1 = new MemberInit();
+
+        // 메서드 이용한 초기화 1
+        member1.InitMember("user1", 15 , 90);
+
+        MemberInit member2 = new MemberInit();
+
+        // 메서드 이용한 초기화 2
+        member2.InitMember("user2" , 16 , 80);
+
+        MemberInit[] members = {member1, member2};
+
+        for(MemberInit s : members){
+            System.out.println("이름: " + s.name + " 나이: " + s.age + " 성적: " + s.grade);
+        }
+    }
+}
