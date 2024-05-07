107f55de718cf5b5786f5b2e6c0e48a72149ad4f
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-05-07 16:53:38 +0900

    Create Robot.java

---
+++ magicNumber/after/Robot.java
@@ -0,0 +1,27 @@
+package magicNumber.after;
+
+public class Robot {
+
+    private String name;
+
+    public static final int COMMAND_WALK = 0;
+    public static final int COMMAND_STOP = 1;
+    public static final int COMMAND_JUMP = 2;
+
+
+    public Robot(String name){
+        this.name = name;
+    }
+
+    public void order(int command){
+        if(command == COMMAND_WALK){
+            System.out.println(name + " walk");
+        } else if (command == COMMAND_STOP) {
+            System.out.println(name + " STOP");
+        } else if (command == COMMAND_JUMP) {
+            System.out.println(name + " jump");
+        }else{
+            System.out.println("error");
+        }
+    }
+}
