5a5d3a99a07143d339793fc504fec1cda6e69012
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-05-09 18:31:01 +0900

    Create PolyMain.java

---
+++ poly.basic/PolyMain.java
@@ -0,0 +1,27 @@
+package poly.basic;
+
+public class PolyMain {
+
+    public static void main(String[] args) {
+
+        // 부모 변수가 부모 인스턴스를 참조
+        System.out.println("Parent -> Parent");
+        Parent parent = new Parent();
+        Parent.parentMethod();
+
+        // 부모 변수가 부모 인스턴스를 참조
+        System.out.println("Child -> Child");
+        Child child = new Child();
+        child.parentMethod();
+        child.childMethod();
+
+        // 부모 변수가 자식 인스턴스 참조(다형적 참조) -> 부모는 자식을 품을 수 있다.
+        System.out.println("Parent -> child");
+        Parent poly = new Child();
+        poly.parentMethod();
+        // poly.childMethod(); 자식의 기능을 호출할 수 없다.
+        // Child child1 = new Parent(); 자식은 부모를 담을 수 없다.
+
+
+    }
+}
