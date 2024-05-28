9d46e461975ff9ba296101bbbcf19d92e6449b79
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-05-28 18:09:48 +0900

    Create PayService.java

---
+++ poly.basic/ex/pay1/PayService.java
@@ -0,0 +1,15 @@
+package poly.ex.pay1;
+
+public class PayService {
+
+    private Pay pay;
+
+    public void processPay(String option, int amount){
+        boolean result;
+        System.out.println("결제를 시작합니다: Option=" + amount + ", Amount=" + amount);
+
+
+
+
+    }
+}
