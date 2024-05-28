d614965702bfdeff4c099d40ef52208bda8454c9
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-05-28 18:06:08 +0900

    Create Model3Car.java

---
+++ poly.basic/car0/Model3Car.java
@@ -0,0 +1,13 @@
+package poly.car0;
+
+public class Model3Car {
+    public void startEngin(){
+        System.out.println("Model3Car.startEngine");
+    }
+    public void offEngine(){
+        System.out.println("Model3Car.offEngine");
+    }
+    public void pressAccelerator(){
+        System.out.println("Model3Car.pressAccelerator");
+    }
+}
