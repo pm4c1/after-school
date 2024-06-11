b7bfc4295b8ed672a6efb94a87d7553bdf1dbc73
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-06-11 18:08:48 +0900

    Create SortTest.java

---
+++ .Lv3_test/TeacherCode/SortTest.java
@@ -0,0 +1,34 @@
+package poly.sort;
+
+import java.util.Scanner;
+
+public class SortTest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // 객체화 부분 작성 1
+        Sort[] sort = {new BubbleSort(), new HeapSort(), new QuickSort()};
+
+        int[] nums = new int[5];
+
+
+        System.out.println("정렬 방식을 선택하세요.");
+        System.out.println("[1] : BubbleSort");
+        System.out.println("[2] : HeapSort");
+        System.out.println("[3] : QuickSort");
+
+        int choice = sc.nextInt();
+
+        // 호출하는 부분 작성 2
+        selectedSort(sort[choice-1], nums);
+
+    }
+
+    private static void selectedSort(Sort sort, int[] nums){
+
+        sort.ascending(nums);
+        sort.descending(nums);
+        sort.description();
+    }
+}
