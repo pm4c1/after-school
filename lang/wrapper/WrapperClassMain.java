321dd65f4c7f18e6a4a0a913b2736b0d2bceda63
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-06-25 16:52:53 +0900

    24.06.25

---
+++ lang/wrapper/WrapperClassMain.java
@@ -0,0 +1,12 @@
+package lang.wrapper;
+
+public class WrapperClassMain {
+
+    public static void main(String[] args) {
+        // Integer newInteger = new Integer(10);
+        Integer newInteger = 10;
+        Integer integerObj = Integer.valueOf(10);
+
+        Long newLong = 100L;
+    }
+}
