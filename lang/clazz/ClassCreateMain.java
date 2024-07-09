e2289e6b69959696e3453d45629eaf0415f03ba0
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-09 17:07:23 +0900

    Update ClassCreateMain.java

--- lang/clazz/ClassCreateMain.java
+++ lang/clazz/ClassCreateMain.java
@@ -8,7 +8,6 @@ public class ClassCreateMain {
         Hello hello = (Hello)helloClass.getDeclaredConstructor().newInstance();
         // getDeclaredConstructor() : 생성자를 선택
         // newInstance() : 선택된 생성자를 기반으로 인스턴스를 생성한다.
-
         String result = hello.hello();
         System.out.println("result = " + result);
 
