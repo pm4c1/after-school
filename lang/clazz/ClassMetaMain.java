7532a03bd489792c98645bdc9bf56062ad650fd2
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-09 17:06:37 +0900

    Update ClassMetaMain.java

--- lang/clazz/ClassMetaMain.java
+++ lang/clazz/ClassMetaMain.java
@@ -26,3 +26,22 @@ public class ClassMetaMain {
 
         for (Class i : interfaces)
             System.out.println("Interface: " + i.getName());
+      }
+}
+/*
+    Class 클래스의 주요 기능
+    1. 타입 정보 얻기 : 클래스의 이름, 슈퍼클래스, 인터페이스, 접근 제한자 등과 같은 정보를 조회할 수 있다.
+    2. 리플렉션 : 클래스에 정의된 메서드, 필드, 생성자 등을 조회하고, 이들을 통해 객체 인스턴스를 생성하거나,
+                 메서드를 호출하는 등의 작업을 할 수 있다.
+                 System.out.println("sumPrimitive = " + sumPrimitive);
+    3. 동적 로딩과 생성 : Class.forName() 메서드를 사용하여 클래스를 동적으로 로드하고, newInstance() 메서드를 통해
+                        새로운 인스턴스를 생성할 수 있다.
+       참고(동적 로딩) - 런타임에 클래스를 로드하는 프로세스를 말함.
+    4. 애노테이션 처리 : 클래스에 적용된 애노테이션을 조회하고 처리하는 기능을 제공한다.
+
+    Class 클래스의 주요 기능
+    1. getDeclaredFields() : 클래스의 모든 필드를 조회한다.
+    2. getDeclaredMethods() : 클래스의 모든 메서드를 조회한다.
+    3. getSuperclass() : 클래스의 부모 클래스를 조회한다.
+    4. getInterfaces() : 클래스의 인터페이스를 조회한다.
+ */
