1aee0a0cc64556e6c46c628f1316b4f315028855
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-09 17:05:30 +0900

    Update ClassMetaMain.java

--- lang/clazz/ClassMetaMain.java
+++ lang/clazz/ClassMetaMain.java
@@ -7,3 +7,8 @@ public class ClassMetaMain {
     public static void main(String[] args) throws Exception {
 
         Class clazz = String.class; // 클래스에서 조회
+      
+        // 모든 필드 출력
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields)
+            System.out.println("Field: " + field.getType() + " " + field.getName());
