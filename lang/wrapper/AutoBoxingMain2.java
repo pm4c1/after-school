ec3887ca180ba4ed41431bcaf84b0ba93380ea6b
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-09 17:03:31 +0900

    Create AutoBoxingMain2.java

---
+++ lang/wrapper/AutoBoxingMain2.java
@@ -0,0 +1,7 @@
+package lang.wrapper;
+
+public class AutoBoxingMain2 {
+    public static void main(String[] args) {
+        // Primitive -> Wrapper
+        int value = 7;
+        Integer boxedValue = value;
