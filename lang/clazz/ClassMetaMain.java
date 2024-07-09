2ceb9ea54c75f3b745e4c237c3a5f64e37e94a87
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-09 17:05:18 +0900

    Update ClassMetaMain.java

--- lang/clazz/ClassMetaMain.java
+++ lang/clazz/ClassMetaMain.java
@@ -3,3 +3,7 @@ package lang.clazz;
 import java.lang.reflect.*;
 
 public class ClassMetaMain {
+  
+    public static void main(String[] args) throws Exception {
+
+        Class clazz = String.class; // 클래스에서 조회
