bdc548189a2bea7bf590ab9ef3fb3adc31fd2a1c
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-11 18:32:28 +0900

    Create Grade.java

---
+++ enumeration/ex3/Grade.java
@@ -0,0 +1,13 @@
+package enumeration.ex3;
+
+public enum Grade {
+    /*
+    enum 선언 방법
+
+    ↓ 이거를
+    public static final ClassGrade BASIC = new ClassGrade();
+    public static final ClassGrade GOLD = new ClassGrade();
+    public static final ClassGrade DIAMOND = new ClassGrade();
+    ↓ 이런식으로 생략할 수 있음 */
+    BASIC, GOLD, DIAMOND
+}
