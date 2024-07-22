3d753c088e422a53b3e1dfc86442f1669a081356
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-22 14:44:05 +0900

    Create EmployeeType.java

---
+++ .Lv4_test/answer/EmployeeType.java
@@ -0,0 +1,18 @@
+package enumeration.test.ex2;
+
+public enum EmployeeType {
+    // enum 클래스
+    FULL_TIME(1.0),
+    CONTRACT(0.8),
+    INTERN(0.5);
+
+    private final double salaryMultiplier;
+
+    EmployeeType(double salaryMultiplier) {
+        this.salaryMultiplier = salaryMultiplier;
+    }
+
+    public double adjustBaseSalary(double baseSalary){
+        return baseSalary * salaryMultiplier;
+    }
+}
