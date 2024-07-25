1dc4da22fa5ba54625f828a269e15baae3549bb6
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-07-25 15:08:24 +0900

    Create CoffeeByNickname.java

---
+++ generics/Coffee/CoffeeByNickname.java
@@ -0,0 +1,14 @@
+package generics.coffee;
+
+public class CoffeeByNickname {
+
+    public String nickName;
+
+    public CoffeeByNickname(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void ready() {
+        System.out.println("커피 준비 완료 : " + nickName);
+    }
+}
